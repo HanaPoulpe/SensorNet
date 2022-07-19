c2ad3d95c9eefa02f056305221fe687467769d38
Hana Burtin <hana@hanapoulpe.org>
2022-07-19 10:22:50 +0200

    Cron unittest (#5)
    * TDD Setup
    
    Defines a complete implementation. Tests are not implemented yet.
    
    * Versioneer Setup
    
    - Adding versioneer to package setup to support version changes.
    
    * Type definitions:
    
    Added:
    - SensorData
    - BackendDriver
    - get_driver
    - BackendError
    - BackendWriteError
    
    * SQLAlchemyDriver Implementation
    
    - Implemented SQLAlchemy driver
    - Completed unittests
    
    * Correct Typo in README.md
    
    * Correct README.md
    Implemented Configuration facade
    
    * Added Errors:
    - ConfigError
    - BackendConfigError
    
    Implemented:
    - Stubber backend for tests
    
    Prepared:
    - Configuration YAML files (empty) for TDD setup of test_configuration.py
    
    * Added Errors:
    - ConfigError
    - BackendConfigError
    - MissingDriverError
    
    Implemented:
    - Stubber backend for tests
    - Unittests for configuration
    
    Prepared:
    - Configuration YAML files
    
    * Added Errors:
    - ConfigError
    - BackendConfigError
    
    Implemented:
    - Stubber backend for tests
    - IP Address Iterators
    - Configuration
    - backend driver type guard
    - plugin loader
    
    Dependency:
    + PyYAML
    
    * Implemented:
    - Configuration CRON Check + Unit test
    - Cron trigger test
    
    Dependency:
    + croniter

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -1,16 +1,31 @@
 """Tests that cron polling configuration works properly."""
+import datetime
 import unittest
+
+import sensor_net.cron as cron
 
 
 class TestCron(unittest.TestCase):
     """Tests cron response."""
     def test_cron_poll(self):
         """Tests that cron is polling correctly."""
-        raise NotImplementedError("Cron is not implemented.")
+        date = datetime.datetime(2022, 1, 1, 1, 1, 55)
+        cron_exp = "1 * * * *"
+
+        self.assertTrue(
+            cron.is_triggered(cron_exp, date),
+            "Cron should be triggered.",
+        )
 
     def test_cron_no_poll(self):
         """Tests that cron is not polling when polling should be skipped."""
-        raise NotImplementedError("Cron is not implemented.")
+        date = datetime.datetime(2022, 1, 1, 1, 0, 55)
+        cron_exp = "1 * * * *"
+
+        self.assertFalse(
+            cron.is_triggered(cron_exp, date),
+            "Cron should not be triggered.",
+        )
 
 
 if __name__ == '__main__':
