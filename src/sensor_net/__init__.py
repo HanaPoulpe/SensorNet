87f8a7cfd18819cf7bdef711296b48ce8ee8d6fc
Hana <hana@hanapoulpe.org>
2022-08-28 13:51:32 +0200

    Ignore __main__ in __init__.py

--- src/sensor_net/__init__.py
+++ src/sensor_net/__init__.py
@@ -5,6 +5,6 @@ from .types import SensorData, sensor_data_guard
 __version__ = _version.get_versions()['version']
 __all__ = ['SensorData', 'sensor_data_guard']
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     from .cmdline import main
     main()
