8fe4eca9c0828ea2a5d2de45ab63338472acfe53
Hana <hana@hanapoulpe.org>
2022-08-28 11:40:46 +0200

    Adds main to the module root to allow ```python -m sensor_net``` run

--- src/sensor_net/__init__.py
+++ src/sensor_net/__init__.py
@@ -1,6 +1,10 @@
 """Contains all common modules required for sensornet to work."""
 from . import _version
 from .types import SensorData, sensor_data_guard
+from .cmdline import main
 
 __version__ = _version.get_versions()['version']
-__all__ = ['SensorData', 'sensor_data_guard']
+__all__ = ['SensorData', 'sensor_data_guard', 'main']
+
+if __name__ == '__main__':
+    main()
